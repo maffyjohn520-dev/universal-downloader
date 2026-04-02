c11d42daacd19519b211afa9151792f2e224c77a
maffyjohn520-dev <maffyjohn520@gmail.com>
2026-04-02 02:15:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -64,5 +64,5 @@ app.post('/download', async (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
+    const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
